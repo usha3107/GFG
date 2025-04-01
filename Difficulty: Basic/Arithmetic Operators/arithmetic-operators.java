c99b3942782009674bbb3da5109699bb68737825
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-04-01 07:58:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.java
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        // Perform all the operations and print in a single line
+        System.out.print(x+y+" ");
+        System.out.print(x-y+" ");
+        System.out.print(x*y+" ");
+        System.out.print(x/y+" ");
+        System.out.print(x%y+" ");
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
