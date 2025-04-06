1469e4792fdaa9a68a786aeac997088ad0e951e1
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-04-06 07:56:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Compute Sum and Print
+         int sum = n*(n+1)/2;
+        System.out.print(sum);
+
+
+
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
