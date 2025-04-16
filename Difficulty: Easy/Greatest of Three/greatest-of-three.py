482f3dd0ca0c32faa88695b8d4c83e8a3f2d03af
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-04-16 09:06:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Greatest of Three/greatest-of-three.py
@@ -0,0 +1,25 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+# Take a, b and c input and print the greatest of three
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a > b and a > c:
+    print(a)
+elif(b > a and b > c):
+    print(b)
+else:
+    print(c)
+
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
