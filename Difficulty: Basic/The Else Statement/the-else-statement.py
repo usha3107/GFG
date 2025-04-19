ffba2c41d62ecef9fd3c72e159476e01ee7bf0ee
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-04-19 08:25:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.py
@@ -0,0 +1,18 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+a = int(input())
+if a>100:
+    print("Big")
+else:
+    print("Number")
+
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
