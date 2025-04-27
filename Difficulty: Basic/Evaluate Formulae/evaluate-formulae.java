00924ab415a01085195d1a03306fa56d0a6b2b52
Ushodaya. Kilari <23mh1a05h8@acoe.edu.in>
2025-04-27 09:43:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Evaluate Formulae/evaluate-formulae.java
@@ -0,0 +1,24 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        int d = scn.nextInt();
+         int sum = (((a+b)/c)+d);
+        System.out.print(sum);
+
+
+
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
